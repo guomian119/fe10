2e371e67d93ec35f86344596818eb79e6d6dbacd
guomian119 <2381503039@qq.com>
2017-10-19 09:44:28 +0800

    aaa

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -7,15 +7,16 @@
     <router-link exact to="/">home主页</router-link>
     <router-link to="/work" active-class="starkwang">我的工作</router-link>
     <router-link to="/stark" tag="li">我的Stark</router-link>
-    <router-link to="/User/2/profile" tag="li">我是profile组件</router-link>
-    <router-link to="/User/4/posts" tag="li">我是posts组件</router-link>
+    <router-link to="/user/2/profile" tag="li">我的profile</router-link>
+    <router-link to="/user/4/posts" tag="li">我的posts</router-link>
+    
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app' 
+  name: 'app'
 }
 </script>
 
--- my-project/src/components/Stark.vue
+++ my-project/src/components/Stark.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-            <h2>hi vue</h2>
+            <h2>hi shudong</h2>
     </div>
 </template>
 
